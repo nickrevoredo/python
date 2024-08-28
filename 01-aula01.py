e1bf17d1d2de08387bd7b65f147793018fd1278e
nick r. <laaurarevoredoo@gmail.com>
2024-08-28 17:25:35 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,21 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Nick") # comentário a linha do código
+print('Nick')
+print('Nick "Nicky" Revoredo')
+print('Nick "Nicky" Revoredo') 
+print("Nick \"Nicky\" Revoredo")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
+
