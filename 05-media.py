eaf765146ac81515f8848d2adf03447884a0fd69
nick r. <laaurarevoredoo@gmail.com>
2024-09-11 17:39:59 -0300

    Add files via upload

---
+++ 05-media.py
@@ -0,0 +1,8 @@
+print("Algoritmo do cáuculo da média")
+nota1 = float(input("Digite a nota1: "))
+nota2 = float(input("Digite a nota2: "))
+nota3  = float(input("Digite a nota3: "))
+nota4 = float(input("Digite a nota4: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f"A média final é {media:.2f}")
