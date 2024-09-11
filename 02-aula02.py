93e4cc4d0fe92a05e13b9b57a1297287bc8b5866
nick r. <laaurarevoredoo@gmail.com>
2024-09-11 17:39:16 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# Atribuição de variáveis
+nome = "Nick"
+idade = "18"
+altura = "1.65"
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade: ", idade, "altura: ", idade ,)
+#Formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
