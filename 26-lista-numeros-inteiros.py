1a24aa3f7ba627a9fcedd3b1c92e5089cc5cd161
nick r. <laaurarevoredoo@gmail.com>
2024-11-06 15:39:37 -0300

    Add files via upload

---
+++ 26-lista-numeros-inteiros.py
@@ -0,0 +1,17 @@
+minhalista = []
+
+minhalista.append (20)
+minhalista.append (21)
+minhalista.append (22)
+minhalista.append (23)
+minhalista.append (24)
+
+menor = min(minhalista)
+maior = max(minhalista)
+soma = sum(minhalista)
+print(f"O menor número da lista: {menor}")
+print(f"O maior número da lista: {maior}")
+print(f"A soma dos números da lista: {soma}")
+
+for lista in minhalista:
+    print(lista)
